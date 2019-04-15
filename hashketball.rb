fef51d962e4850774a6fe463c4916699fbca6386
Joseph Reilly <reilly4181@gmail.com>
2019-04-15 07:47:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,7 +154,7 @@ end
 
 def team_colors(team_name)
   hash = game_hash
-  array = []
+ 
   hash.each do |location, attributes|
     if hash[location].values.include?(team_name)
       attributes.each do |attribute, info|
